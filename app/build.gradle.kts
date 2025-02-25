4bc146878e6d048e1c8592ae9ce925ac5fcea0a3
lyuhyewon <lyuhw11023@gmail.com>
2025-02-25 19:47:20 +0900

    Splash화면 기본 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -55,4 +57,7 @@ dependencies {
 
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Splash
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
--- app/src/main/java/com/example/coinapp/App.kt
+++ app/src/main/java/com/example/coinapp/App.kt
@@ -5,7 +5,6 @@ import timber.log.Timber
 
 class App : Application() {
 
-
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
