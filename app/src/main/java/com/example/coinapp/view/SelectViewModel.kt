51ed6cf24b06946710daf4c68e43f44d761559ce
lyuhyewon <lyuhw11023@gmail.com>
2025-02-26 16:25:24 +0900

    코인 선택 API 데이터 가공

--- app/src/main/java/com/example/coinapp/view/SelectViewModel.kt
+++ app/src/main/java/com/example/coinapp/view/SelectViewModel.kt
@@ -2,18 +2,40 @@ package com.example.coinapp.view
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.coinapp.dataModel.CurrentPrice
+import com.example.coinapp.dataModel.CurrentPriceResult
+import com.example.coinapp.network.model.CurrentPriceList
 import com.example.coinapp.repository.NetWorkRepository
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class SelectViewModel : ViewModel() {
 
     private val netWorkRepository = NetWorkRepository()
+
+    private lateinit var currentPriceResultList : ArrayList<CurrentPriceResult>
     fun getCurrentCoinList() = viewModelScope.launch {
 
         val result = netWorkRepository.getCurrentCoinList()
 
-        Timber.d(result.toString())
+        currentPriceResultList = ArrayList()
+
+        for(coin in result.data) {
+
+            try{
+                val gson = Gson()
+                val gsonToJson = gson.toJson(result.data.get(coin.key))
+                val gsonFromJson = gson.fromJson(gsonToJson, CurrentPrice::class.java)
+
+                val currentPriceResult = CurrentPriceResult(coin.key, gsonFromJson)
+
+                currentPriceResultList.add(currentPriceResult)
+
+            }catch (e: java.lang.Exception){
+                Timber.d(e.toString())
+            }
+        }
     }
 
 }
